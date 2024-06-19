6b7560d92c92315b7f41cd8274ab48b51982d399
Asianhavoc <miguel.orias02@gmail.com>
2024-06-20 03:01:19 +0800

    .

--- config/constants.php
+++ config/constants.php
@@ -2,5 +2,5 @@
 	define('DB_HOST', 'localhost');
 	define('DB_USER', 'root');
 	define('DB_PASS', ''); // change to your password
-	define('DB_NAME', 'umpays_db'); 
+	define('DB_NAME', '_db'); // name of database
 ?>
