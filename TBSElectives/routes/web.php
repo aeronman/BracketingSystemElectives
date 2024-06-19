3e3eb49b77f32843a2c2710b8abbf433232ab567
ChesterMapagmahal0120 <161694981+ChesterMapagmahal0120@users.noreply.github.com>
2024-06-19 22:31:19 +0800

    Laravel with auth
    added features
    authentication
    user
    admin

--- TBSElectives/routes/web.php
+++ TBSElectives/routes/web.php
@@ -1,9 +1,28 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
+//HomeController
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
-    return view('dbconn');
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+//route for the homecontroller for Admin Dashboard
+
+Route::get('admin/dashboard', [HomeController::class,'index'])->Middleware(['auth', 'admin']);
