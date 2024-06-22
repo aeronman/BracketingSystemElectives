f203987cc8506fd8e3f21b79de55ef392ca2a0b3
Asianhavoc <miguel.orias02@gmail.com>
2024-06-22 13:13:31 +0800

    di na kailangan

--- config/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require 'constants.php';
-
-	$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-	if ($connection) { 
-		//echo"Has connection <br>"; 
-	}
-?>
--- config/constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', ''); // change to your password
-	define('DB_NAME', '_db'); // name of database
-?>
