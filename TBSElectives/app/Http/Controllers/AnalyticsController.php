caac8e5b7dd9d73c0df1e81972cb303b083cd881
Asianhavoc <miguel.orias02@gmail.com>
2024-06-22 15:53:06 +0800

    anaco

--- TBSElectives/app/Http/Controllers/AnalyticsController.php
+++ TBSElectives/app/Http/Controllers/AnalyticsController.php
@@ -4,7 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Team;
+use App\Models\Player;
+use Illuminate\Support\Facades\Auth;
+
 class AnalyticsController extends Controller
 {
-    //
+
+    public function index()
+    {
+        return view('admin/analytics');
+    }
 }
